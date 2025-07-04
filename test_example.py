67845ef7e1b6680ebdbc76ad8889c6de0534333f
dgh06175 <dgh06175@gmail.com>
2025-07-04 10:47:55 +0900

    feat: demo.playwright UI test

--- test_example.py
+++ test_example.py
@@ -1,11 +1,14 @@
 import re
+
 from playwright.sync_api import Page, expect
+
 
 def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
+
 
 def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/")
@@ -15,3 +18,15 @@ def test_get_started_link(page: Page):
 
     # Expects page to have a heading with the name of Installation.
     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+
+def test_example(page: Page) -> None:
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    expect(page.get_by_role("heading")).to_contain_text("todos")
+    expect(page.get_by_role("textbox", name="What needs to be done?")).to_be_visible()
+    page.get_by_role("textbox", name="What needs to be done?").click()
+    page.get_by_role("textbox", name="What needs to be done?").fill("pytest")
+    page.get_by_role("textbox", name="What needs to be done?").press("Enter")
+    page.get_by_role("checkbox", name="Toggle Todo").check()
+    expect(page.get_by_role("button", name="Clear completed")).to_be_visible()
+    expect(page.locator("body")).to_contain_text("0 items")
